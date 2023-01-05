65376d2b4082808dda597dac084146e7240f93b2
Vedran Erak <vedran1.erak1@gmail.com>
2023-01-05 16:39:58 +0200

    Deleted 2 unused imports

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 import { Variables } from '../styles/theme'
 import SvgCheckMarkSvgrepoCom from './svg/CheckMarkSvgrepoCom'
